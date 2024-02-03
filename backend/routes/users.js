ba79e00ee53ce27947d288fcc6b3a02ef81775a4
Theoryofrelativity1915 <eliengelhart@gmail.com>
2024-02-03 11:04:22 -0500

    Fixed bcrypt dependency.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,18 +1,18 @@
 const express = require('express')
-const {pool} = require('../db.js')
-const bcrypt = require('bcrypt')
+const { pool } = require('../db.js')
+const bcrypt = require('bcryptjs')
 const paginatedResults = require('../middleware/paginatedResults')
 const { getPersonnel, assignPersonnel } = require('../utils/utils')
 const router = express.Router()
 
 router.get('/', getPersonnel, paginatedResults, (req, res) => {
-    res.setHeader('Content-Type', 'application/json')
-    res.status(200).send({"results" : res.paginatedResults, "totalPages": res.totalPages})
+  res.setHeader('Content-Type', 'application/json')
+  res.status(200).send({ "results": res.paginatedResults, "totalPages": res.totalPages })
 })
 
 router.put('/assign', assignPersonnel, (req, res) => {
-    res.setHeader('Content-Type', 'application/json')
-    res.status(200).send('Good request.')
+  res.setHeader('Content-Type', 'application/json')
+  res.status(200).send('Good request.')
 })
 
 
